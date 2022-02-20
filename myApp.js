87738d29032c8be3b7919365ee549ba76ebc36db
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-02-20 07:06:11 -0800

    removed MongoClient

--- myApp.js
+++ myApp.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const { MongoClient } = require("mongodb");
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
